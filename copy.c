f2c44676d641afa3865571c95aab435c9eb39afa
Tarokh Yaghoubi <tarokhgit@gmail.com>
2024-02-11 16:22:58 +0330

    a program that repeats what you say

---
+++ copy.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#define BUFFSIZE    4096
+
+int
+main(void)
+{
+    int     n;
+    char    buf[BUFFSIZE];
+
+    while ((n = read(STDIN_FILENO, buf, BUFFSIZE)) > 0)
+        if (write(STDOUT_FILENO, buf, n) != n)
+            perror("write: error");
+
+    if (n < 0)
+        perror("read: error");
+
+    exit(0);
+
+}
+
