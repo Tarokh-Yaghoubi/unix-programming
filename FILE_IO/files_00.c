19aad8fcac1b23a87f7c1a58a7ebd814bc01d5f1
Tarokh Yaghoubi <tarokhgit@gmail.com>
2024-06-09 18:14:07 +0330

    playing with the open system call

--- FILE_IO/files_00.c
+++ FILE_IO/files_00.c
@@ -3,14 +3,29 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdlib.h>
 
 int
 main(int argc, char* argv[]) {
 	int fd = 0;
-	fd = open("/home/Desktop/unix-programming/trash_file", O_RDONLY);
-	if (fd == -1)
+	fd = open("/home/tarok/Desktop/unix-programming/FILE_IO/trash_file", O_RDWR | O_CREAT);
+	if (fd == -1) {
 		perror("open");
+		return -1;
+	}
 
 	return EXIT_SUCCESS;
+}
+
+int open_trunc(void) {
+	int fd = 0;
+	fd = open("/home/tarok/Desktop/unix-programming/trash_file", O_WRONLY | O_CREAT | O_TRUNC);
+	if (fd == -1) {
+		perror("open");
+		return -1;
+	}
+
+	return EXIT_SUCCESS;
+
 }
 
