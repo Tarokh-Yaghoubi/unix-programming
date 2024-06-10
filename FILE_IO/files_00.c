e587a53987d47aae0f1e3271fb4d7246bc8ddbad
Tarokh Yaghoubi <tarokhgit@gmail.com>
2024-06-10 13:11:09 +0330

    played with open and creat syscalls | Linux programming

--- FILE_IO/files_00.c
+++ FILE_IO/files_00.c
@@ -4,28 +4,98 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+
+
+static char* FILE_NAME = "trash_file";
+static char* CREAT_FILE = "creat_file";
+
+int open_trunc(void);
+int open_file_for_read(void);
+int creat_syscall(void);
+int creat_syscall_userspace(const char*, int);
 
 int
 main(int argc, char* argv[]) {
 	int fd = 0;
-	fd = open("/home/tarok/Desktop/unix-programming/FILE_IO/trash_file", O_RDWR | O_CREAT);
+	fd = open("trash_file", O_RDWR | O_CREAT | O_EXCL);
 	if (fd == -1) {
+		if (errno == EEXIST) {
+			printf("%s\n", "file already exists");
+		}
 		perror("open");
-		return -1;
+		return EXIT_FAILURE;
 	}
 
+	creat_syscall();
 	return EXIT_SUCCESS;
 }
 
-int open_trunc(void) {
+int 
+open_trunc(void) {
 	int fd = 0;
-	fd = open("/home/tarok/Desktop/unix-programming/trash_file", O_WRONLY | O_CREAT | O_TRUNC);
+	fd = open("trash_file", O_WRONLY | O_CREAT | O_TRUNC);
 	if (fd == -1) {
 		perror("open");
-		return -1;
+		return EXIT_FAILURE;
 	}
 
 	return EXIT_SUCCESS;
 
+}
+
+// you must provide the mode argument of open system call when passing O_CREAT
+// If you forget to pass the mode arg, quite ugly things may happen, btw the result
+// is undefined.
+
+// Permissions (mode) are te same as the UNIX permission bitset.
+// But for more portability, POSIX has provided some sort of constants.
+// this CONSTANTS can be binary-ORed together and supply the mode argument
+
+// umask is modifiable via the umask() call
+
+int 
+open_file_for_read(void) {
+	int fd = 0;
+	fd = open(FILE_NAME, O_WRONLY | O_CREAT | O_TRUNC, 
+			S_IRWXU | S_IWGRP | S_IRGRP | S_IROTH);
+	if (fd == -1) {
+		printf("%s [%d]\n", "The file descriptor ret_val is ---->>> ", fd);
+		perror("open");
+		return EXIT_FAILURE;
+
+	}
+
+	// this is doing the same thing, we are trading portability for readability
+	// fd = open(FILE_NAME, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+}
+
+
+// The combination of O_WRONLY | O_CREAT | O_TRUNC is so common that a syscall exists to provide
+// just that behavior
+
+int
+creat_syscall(void) {
+	int fd = 0;
+	fd = creat(CREAT_FILE, 0744);
+	if (fd == -1) {
+		printf("%s [%d] \n", "The file descriptor ret_val is ----->>>> ", fd);
+		perror("creat");
+		return EXIT_FAILURE;
+	} else {
+		printf("%s\n%s [%s]\n", "file created successfully", "file name ", CREAT_FILE);
+		return EXIT_SUCCESS;
+	}
+
+}
+
+
+int
+creat_syscall_userspace(const char* name, int mode) {
+
+	return open(name, O_WRONLY | O_CREAT | O_TRUNC, mode);
 }
 
